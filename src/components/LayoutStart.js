fc1ee958ce3739dadeea3c6d047da17dac4ada65
bycaroline <eklund.caroline@gmail.com>
2024-05-16 17:32:29 +0200

    server connection established

--- src/components/LayoutStart.js
+++ src/components/LayoutStart.js
@@ -4,44 +4,59 @@ import { useLocation } from 'react-router-dom'
 import styles from '../css/styles.module.css'
 import GarnGalore from '../assets/GarnGalore.jpg';
 import ProjectCards from './ProjectCards';
+import React, { useState, useEffect } from 'react';
+import profilePhoto from '../assets/profilePhoto.png';
+import { sendRequest } from './HttpHandler';
+import Paragraph2 from './Paragraph2';
+
 
 const LayoutStart = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const responseData = await sendRequest(
+                    `http://localhost:8080/projects`,
+                    'GET',
+                    null,
+                );
+                // Set the fetched projects in state
+                setProjects(responseData);
+            } catch (error) {
+                console.error('Error fetching projects:', error);
+            }
+        };
+        fetchProjects();
+    }, []);
+
 
     return (
         <>
             <div className={styles.wrapper}>
                 <DashHeader />
-
                 <div className={styles.wrapperTextIntro}>
-                    <div className={styles.hello}>
-                        <h1>Hi there!</h1>
-                    </div>
-                    <div className={styles.shortAbout}>
+                    <div className={styles.wrapperParagraph1}>
                         <div>
-                            {/* get text from backend */}
-                            <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                when an unknown printer took a galley of type and scrambled it to make a
-                                type specimen book. It has survived not only five centuries, but also</p>
+                            <img className={styles.imgHeader} src={profilePhoto} alt="" />
                         </div>
                         <div>
-                            <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                                been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                It has survived not only five centuries, but also</p>
+                            <h1>Hi,</h1><h1>I am a developer</h1>
                         </div>
                     </div>
-
+                    <Paragraph2 />
                 </div>
-                <main className={styles.publicRecipeItem}>
-                    <div className={styles.row}>
-                        {/* loop over 4 projects here */}
-                        <ProjectCards />
-                        <ProjectCards />
-                    </div>
-                    <div className={styles.row}>
-                        <ProjectCards />
-                        <ProjectCards />
-                    </div>
-                </main >
+
+                <section className={styles.wrapperProjects}>
+                    {projects.map((project, index) => (
+                        index % 2 === 0 ? (
+                            <div key={index} className={styles.projectRow}>
+                                <ProjectCards project={projects[index]} />
+                                {index + 1 < projects.length && <ProjectCards project={projects[index + 1]} />}
+                            </div>
+                        ) : null
+                    ))}
+                </section>
                 <p>eklund.caroline@gmail.com</p>
             </div >
         </>
--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,17 +1,63 @@
 import React from 'react'
 import styles from '../css/styles.module.css'
-import GarnGalore from '../assets/GarnGalore.jpg';
+import { Link } from 'react-router-dom';
 
-function ProjectCard() {
+
+// const ProjectCards = ({ project }) => {
+//     // Assuming your backend server is running on localhost:8080
+//     const backendUrl = 'http://localhost:8080';
+
+//     const MAX_DESCRIPTION_LENGTH = 41;
+
+//     // Truncate description text if it exceeds the maximum length
+//     const truncatedDescription = project.description.length > MAX_DESCRIPTION_LENGTH
+//         ? project.description.substring(0, MAX_DESCRIPTION_LENGTH) + '...'
+//         : project.description;
+
+//     return (
+//         <div className={styles.projectCard}>
+//             {/* Prepend the backend URL to the image name */}
+//             <h2>{project.name}</h2>
+//             <Link to={`/projects/${project.id}`}>
+//                 <img src={`${backendUrl}${project.image}`} alt={`Image ${project.name}`} />
+//             </Link>
+//             <div className={styles.textProjectCard}>
+//                 <p>{truncatedDescription}</p>
+//                 <Link to={`/projects/${project.id}`}>Read more</Link>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProjectCards;
+
+
+
+
+const ProjectCards = ({ project }) => {
+    // Assuming your backend server is running on localhost:8080
+    const backendUrl = 'http://localhost:8080';
+
+    const MAX_DESCRIPTION_LENGTH = 45;
+
+    // Truncate description text if it exceeds the maximum length
+    const truncatedDescription = project.description.length > MAX_DESCRIPTION_LENGTH
+        ? project.description.substring(0, MAX_DESCRIPTION_LENGTH) + '...'
+        : project.description;
+
     return (
         <div className={styles.projectCard}>
-            <img src={GarnGalore} />
+            {/* Prepend the backend URL to the image name */}
+            <h2>{project.name}</h2>
+            <Link to={`/projects/${project.id}`}>
+                <img src={`${backendUrl}${project.image}`} alt={`Image ${project.name}`} />
+            </Link>
             <div className={styles.textProjectCard}>
-                <p>Java team project to create a webshop using... </p>
-                <a href="">Read more</a>
+                <p>{truncatedDescription}</p>
+                <Link to={`/projects/${project.id}`}>Read more</Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProjectCard
+export default ProjectCards;
